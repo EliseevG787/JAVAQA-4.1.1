4a158b56e44da3d3b8e3a7191e66b042129de1e1
unknown <rdn787@bk.ru>
2021-03-01 02:20:12 +0300

    added Smartphone.java

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class Smartphone extends Product {
+
+    private String fabricator;
+
+    public Smartphone(int id, String name, int price, String fabricator) {
+        super(id, name, price);
+        this.fabricator = fabricator;
+    }
+
+    public String getFabricator() {
+        return fabricator;
+    }
+}
